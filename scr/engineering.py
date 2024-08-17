d8e5941af89c4e7840337c15648f18283694dfa4
RIKI OKAMURA <rickey3098@gmail.com>
2024-08-17 16:27:16 +0900

    ある程度特徴量を作成しました。経済力をもう少し因数分解して定義したい。

--- scr/engineering.py
+++ scr/engineering.py
@@ -12,3 +12,33 @@ def make_motivation_gender(gender):
         return 1.0
     elif gender == 'male':
         return 1.3
+
+def make_child01(child):
+    if child == '0_child':
+        return 0
+    else:
+        return 1
+
+def make_TripEasier_ProductPitched(productpitched):
+    if productpitched == 'Basic':
+        return 3
+    elif productpitched == 'Super Deluxe':
+        return 0.001
+    else:
+        return 1
+
+def make_TripEasier_Marry(marry):
+    if marry == 'Single':
+        return 2
+    else:
+        return 1
+
+def make_TripEasier_Child(child):
+    if child == '0_child':
+        return 2
+    elif child == '1_child':
+        return 1
+    elif child == '2_child':
+        return 1
+    else:
+        return 0.4
