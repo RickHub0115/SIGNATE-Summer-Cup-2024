7ae6c94a24021bbf64be3dbc8f23c5c479f01dbf
RIKI OKAMURA <rickey3098@gmail.com>
2024-08-29 00:06:37 +0900

    スタッキングが回りました

--- scr/models/gbdt.py
+++ scr/models/gbdt.py
@@ -9,7 +9,7 @@ class Model1_CatBoost_1:
             #'scale_pos_weight': 497/3489,
             'loss_function': 'Logloss',
             'eval_metric': 'AUC',
-            'iterations': 7,
+            'iterations': 7000,
             'learning_rate': 0.005,
             'depth': 3,
             # 'cat_features': category_columns,
@@ -38,7 +38,7 @@ class Model1_CatBoost_2:
             #'scale_pos_weight': 497/3489,
             'loss_function': 'Logloss',
             'eval_metric': 'AUC',
-            'iterations': 7,
+            'iterations': 7000,
             'learning_rate': 0.005,
             'depth': 5,
             # 'cat_features': category_columns,
@@ -67,7 +67,7 @@ class Model1_CatBoost_3:
             #'scale_pos_weight': 497/3489,
             'loss_function': 'Logloss',
             'eval_metric': 'AUC',
-            'iterations': 7,
+            'iterations': 7000,
             'learning_rate': 0.005,
             'depth': 7,
             # 'cat_features': category_columns,
@@ -117,7 +117,7 @@ class Model1_XGBoost_1:
             dtrain,  
             evals=evals, 
             verbose_eval=100,
-            num_boost_round=7
+            num_boost_round=7000
         )
 
     def predict(self, x):
@@ -152,7 +152,7 @@ class Model1_XGBoost_2:
             dtrain,  
             evals=evals, 
             verbose_eval=100,
-            num_boost_round=7
+            num_boost_round=7000
         )
 
     def predict(self, x):
@@ -187,7 +187,7 @@ class Model1_XGBoost_3:
             dtrain,  
             evals=evals, 
             verbose_eval=100,
-            num_boost_round=7
+            num_boost_round=7000
         )
 
     def predict(self, x):
@@ -221,7 +221,7 @@ class Model1_LightGBM_1:
             dtrain, 
             valid_sets=[dtrain, dval],
             valid_names=['train', 'valid'],
-            num_boost_round=7
+            num_boost_round=7000
         )
 
     def predict(self, x):
@@ -254,7 +254,7 @@ class Model1_LightGBM_2:
             dtrain, 
             valid_sets=[dtrain, dval],
             valid_names=['train', 'valid'],
-            num_boost_round=7
+            num_boost_round=7000
         )
 
     def predict(self, x):
@@ -287,7 +287,7 @@ class Model1_LightGBM_3:
             dtrain, 
             valid_sets=[dtrain, dval],
             valid_names=['train', 'valid'],
-            num_boost_round=7
+            num_boost_round=7000
         )
 
     def predict(self, x):
